fe0b5817103516a6ee4ff57a17d9c5ed03e18016
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2021-06-04 16:57:50 +0200

    AddAssetsAndProjectSettings

---
+++ Assets/Scripts/arrays.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class arrays : MonoBehaviour
+{
+
+    string[] array1 = new string[4];
+    float[] array2 = new float[](1a, 2a, 3a, 4a);
+    int[] array3 = new int[](2, 3, 4, 5);
+
+    //4 Werte überall.
+    // 1) Durchlaufen ausgeben der Werte
+    // 2) Länge des Arrays ausgeben als Debug
+    // 3) 2 Werte direkt ändern
+    // 4) GameObjects Images anlegen, Tag vergeben, zuweisen und Farbe ändern.
+
+}
